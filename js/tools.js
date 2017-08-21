08f9f1b2048ca0d0af8d118ed730ac68fc6f6618
Bijit Soni <bijitsony@gmail.com>
2017-08-21 13:05:43 +0530

    Add Sanitary Ware Calculator
    This is pre-release and development is still in process.

--- js/tools.js
+++ js/tools.js
@@ -79,6 +79,23 @@ window.onload = function() {
       document.getElementById('selectionMsg').innerHTML = "Please select any of options";
     }
   });
+
+  $('#buildingType').on('change', function() {
+    //  alert( this.value ); // or $(this).val()
+    if (this.value == "commercial") {
+      $('#commercial').show();
+      $('#industrial,#institutional').hide();
+    } else if (this.value == "industrial") {
+      $('#industrial').show();
+      $('#commercial,#institutional').hide();
+    } else if (this.value == "institutional") {
+      $('#institutional').show();
+      $('#commercial,#industrial').hide();
+    } else {
+      $('#commercial,#industrial,#institutional').hide();
+      document.getElementById('selectionErr').innerHTML = "Please select any of options";
+    }
+  });
 };
 
 // Function for calculating arc length
@@ -115,4 +132,62 @@ function resultCentreAngle() {
   var positiveResult = toInteger(Math.abs(result));
   // Appending result
   document.getElementById('arcValue').innerHTML = "The result is: " + positiveResult;
+}
+
+
+function calcPax() {
+  // Taking input
+  var a = document.getElementById('carpetArea').valueAsNumber;
+  // Calculating Number of People
+  var result = (a / 4);
+  var positiveResult = Math.abs(result);
+
+  return positiveResult;
+}
+
+function calcMale() {
+  // Calculating Number of Male
+  var a = document.getElementById('percentageOfMale').valueAsNumber;
+  var result = (a / 100) * calcPax();
+  var positiveResult = Math.ceil(result);
+
+  return positiveResult;
+}
+
+function calcFemale() {
+  // Calculating Number of Male
+  var a = document.getElementById('percentageOfMale').valueAsNumber;
+  var result = ((100 - a) / 100) * calcPax();
+  var positiveResult = Math.ceil(result);
+
+  return positiveResult;
+}
+
+function calcUrinals() {
+  // Calculating Number of Urinals
+  var result = calcMale() / 50;
+  var positiveResult = Math.ceil(result);
+
+  return positiveResult;
+}
+
+function calcWCMale() {
+  // Calculating Number of WC for Male
+  var result = calcMale() / 100;
+  var positiveResult = Math.ceil(result);
+
+  return positiveResult;
+}
+
+function calcWCFemale() {
+  // Calculating Number of WC for Male
+  var result = calcFemale() / 100;
+  var positiveResult = Math.ceil(result);
+
+  return positiveResult;
+}
+
+function resultSanitaryWare() {
+  // Declaring Result
+  document.getElementById('resultSanitaryWare').innerHTML = "Urinals: " + calcUrinals() + "<br/>" + "WC for Male:" + calcWCMale() + "<br/>" + "WC for Female:" + calcWCFemale();
 }
