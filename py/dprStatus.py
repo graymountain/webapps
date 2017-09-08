47bc79ab239860218890f4f220e55928b648b8ad
Devang Chauhan <devang@outlook.in>
2017-09-08 15:28:38 +0530

    DPR Status
    Separate list of People to email is provided

--- py/dprStatus.py
+++ py/dprStatus.py
@@ -4,9 +4,20 @@ from datetime import datetime
 
 """The purpose of the script is to generate a report of when was the time a DPR was updated."""
 
+monthDict = {"1":"Jan", "2":"Feb", "3":"Mar", "4":"Apr", "5":"May", "6":"Jun", "7":"Jul", "8":"Aug", "9":"Sep", "10":"Oct", "11":"Nov", "12":"Dec"}
+todayMonth = datetime.now().month
+todayMonth = monthDict[str(todayMonth)]
+todayDate = str(datetime.now().day)
+if len(todayDate) == 1:
+    todayDate = "0" + todayDate
+
+
 fileList = []
 timeList = []
 employeeCount = 0
+
+catchFileList = []
+catchTimeList = []
 
 path = r"Z:\DPR\2017"  # Path to the DPR folder
 dirs = os.listdir(path)
@@ -25,14 +36,30 @@ for folder in dirs:  # For every DPR in the folder extract last modified time
                     catchTime = catchTime[:8] + "0" + catchTime[9:]
                     catchTime = catchTime.replace(" ", ",")
                     timeList.append(catchTime)
+                    day = catchTime[8:10]
+                    month = catchTime[4:7]
+                    if day != todayDate and month != todayMonth:
+                        catchFileList.append(str(file))
+                        catchTimeList.append(catchTime)
+
                 # when the date is not in single digit
                 elif catchTime[8] != " ":
                     catchTime = catchTime.replace(" ", ",")
                     timeList.append(catchTime)
+                    day = catchTime[8:10]
+                    month = catchTime[4:7]
+                    if day != todayDate and month != todayMonth:
+                        catchFileList.append(str(file))
+                        catchTimeList.append(catchTime)
+
 
 result = dict(zip(fileList, timeList))
 keyList = result.keys()
 keyList = sorted(keyList)
+
+catchResult = dict(zip(catchFileList, catchTimeList))
+catchKeyList = catchResult.keys()
+catchKeyList = sorted(catchKeyList)
 
 # Error Report
 if employeeCount == len(keyList):
@@ -58,7 +85,15 @@ try:
     for key in keyList:
         line = key + "," + result[key]
         f.write(line + "\n")
+    f.write("\n")
+    f.write("*******************************************************" + "\n")
+    f.write("\n")
+    f.write("An Email to be sent to following people \n")
+    f.write("\n")
+    for key in catchKeyList:
+        line = key + "," + catchResult[key]
+        f.write(line + "\n")
     f.close()
     print("File written successfully! Yeah!")
 except Exception as e:
-    print("\n" + "Something is wrong! File was not written!" + "\n" +  str(e))
+    print("\n" + "Something is wrong! File was not written!" + "\n" + str(e))
