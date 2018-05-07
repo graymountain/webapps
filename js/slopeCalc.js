c180651cde041df7af83f674e20a7ef8df8a0aa7
Bijit Soni <bijitsony@gmail.com>
2018-05-07 18:20:04 +0530

    Function Removal
    Remove Floor Level function from slope calc

--- js/slopeCalc.js
+++ js/slopeCalc.js
@@ -61,17 +61,3 @@ function distanceBwPoints() {
 
   document.getElementById('distanceBwPoints').innerHTML = "The distance is: " + positiveResult;
 };
-
-
-function floorHeights(startLevel, floors){
-  var heights = [];
-  level = startLevel;
-  for (var no = 0; no < floors; no ++) {
-    level = level + 3000;
-    heights.push(level);
-  }
-  return heights;
-}
-
-var x = floorHeights(3500, 23);
-console.log("The floor levels are " + x);
